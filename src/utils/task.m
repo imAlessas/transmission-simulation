94dbb5d2811f020b28e46cc466ce67865ea8c20d
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-28 19:22:19 +0200

    commented task.m

--- src/utils/task.m
+++ src/utils/task.m
@@ -1,7 +1,18 @@
 function task(n)
-% this function is utilized to display the task divisor in the Command
-% Window: it is purely created in order to increase the readability
+    % TASK displays a header for a specific task identified by the input number.
+    %
+    % INPUT:
+    %   n:    the task number to be displayed in the header.
+    %
+    % USAGE:
+    %   task(1) - Display a header for Task 1.
+
+    % Display two newlines for separation
     disp(newline), disp(newline);
+
+    % Display the header for the specified task number
     disp(" -- -- -- -- -- -- TASK " + n + " -- -- -- -- -- -- ");
+
+    % Display a newline for separation
     disp(newline);
 end
