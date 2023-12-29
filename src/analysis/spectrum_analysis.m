5885972b195df32b60c8d568d9252f6e896d792a
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-29 21:04:23 +0200

    minor update on src\analysis\spectrum_analysis.m

--- src/analysis/spectrum_analysis.m
+++ src/analysis/spectrum_analysis.m
@@ -28,7 +28,7 @@ C_BPSK = C_BASK .* ( exp(1j * k * OMEGA * TAU / 2) -  exp(- 1j * k * OMEGA * TAU
 if RESULT && PLOTS
     % creates figure and settings
     f = figure(1);
-    f.Name = 'Task 7';
+    f.Name = 'Analysis of BPSK spectrum';
     f.NumberTitle = 'off';
     f.Position = [450, 100, 700, 600];
     
