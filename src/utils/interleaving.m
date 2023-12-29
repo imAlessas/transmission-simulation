4ebcee38e5eed7f72adedf35f47121a24b40286d
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2023-12-29 13:52:52 +0200

    created interlaving and deinterlaving functions

--- src/utils/interleaving.m
+++ src/utils/interleaving.m
@@ -1,10 +1,37 @@
-# add padding_bits to round the number to 26. Use the last 6 bits (up to 64 rapresentation) to say how many of the bits are for padding. after de-inerleaving remove padding bits.
-# to know how many bits are for padding: 
+function mixed_sequence = interleaving(unmixed_sequence)
+% INTERLEAVING reorganizes a sequence by interleaving its elements into a matrix.
+%
+% INPUT:
+%   unmixed_sequence:   A vector representing the original sequence to be interleaved.
+%
+% OUTPUT:
+%   mixed_sequence:     A vector representing the interleaved sequence.
 
-padding_bits = number_of_bits_in_the_sequence % 26;
-if padding_bits < 6
-	padding_bits = padding_bits + 26
+    % Define the length of each column (also the number of rows)
+    column_length = 31;
+
+    % Calculate the number of length of each row (also the number of columns) based on the input sequence length
+    row_length = length(unmixed_sequence) / column_length;
+
+    % Write on the columns and read on the rows to create the interleaved matrix
+    interleaver_matrix = [];
+
+    % Iterate through the rows
+    for i = 1 : row_length
+        % Extract the current column from the unmixed sequence
+        current_column = unmixed_sequence(column_length * (i-1) + 1 : column_length * i)';
+        
+        % Append the current column to the matrix
+        interleaver_matrix = [interleaver_matrix, current_column];
+    end
+    
+    % Initialize the interleaved sequence
+    mixed_sequence = [];
+
+    % Iterate through the columns of the matrix
+    for i = 1 : column_length
+        % Append the elements from each row of the current column to the interleaved sequence
+        mixed_sequence = [mixed_sequence, interleaver_matrix(i, 1 : end)];
+    end
+
 end
-
-# convert padding_bits in binary and append the following number:
-zeros for (padding_bits  - 6) times + padding_bits_converted
