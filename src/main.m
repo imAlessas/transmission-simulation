8393da8506087e3346154d7b5903b1137fc7b9b2
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-01-13 20:15:17 +0200

    minor changes

--- src/main.m
+++ src/main.m
@@ -1,5 +1,9 @@
 clc, clearvars, clear, format compact
 
+% Import MATLAB packages
+import communications.*  % used for cyclegen
+
+% Import local packages
 import utils.*
 import func.*
 
@@ -8,7 +12,7 @@ import func.*
 % DEBUG: Additional display for variables or comparisons
 % PLOTS: Toggle for generating plots
 RESULT = 1;  % Set to 1 to display result values
-DEBUG = 0;   % Set to 1 to enable additional debugging display
+DEBUG = 1;   % Set to 1 to enable additional debugging display
 PLOTS = 0;   % Set to 1 to generate plots, 0 to disable plots
 
 
@@ -21,20 +25,23 @@ PLOTS = 0;   % Set to 1 to generate plots, 0 to disable plots
 ALPHABET = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 PROBABILITY_VECTOR = [11, 7, 9, 1, 6, 6, 13, 14, 13, 5, 11, 4]/100;
 
-TAU = 60e-9; % symbol duration time, [s]
+tau = 60e-9; % symbol duration time, [s]
+samples_per_symbol = 500; % samples per symbol
 SNR = 8.1; % Signal-to-Noise-Ration, [dB]
 % Source Code: Shannon-Fano
 % Error correction code: Cyclic
 generation_polynomial = [1 0 0 1 0 1]; % z^5 + z^2 + z^0
-CODEWORD_LENGTH = 31; % m
+codeword_length = 31; % m
 % 
-F_0 = 2.5e+9; % carrier frequency [Hz]
+f0 = 2.5e+9; % carrier frequency [Hz]
 % Modulation: BPSK
-PHASE_SHIFT = pi; % [rad]
+phase_shift = pi; % [rad]
 U = 1; % amplitude BPSK signal [V]
 
 transmitted_symbol_number = 20;
 
+% key used for the scrambling algorithm
+scrambler_key = randi(2, 1, codeword_length) - 1; 
 
 
 
@@ -57,7 +64,7 @@ show(DEBUG, initial_symbol_sequence);
 % = = = = = = = = Task 2 = = = = = = = =
 % Analysis of data source with Shannon-Fano code 
 
-run("analysis\source_data_analysis.m")
+% run("analysis\source_data_analysis.m")
 
 
 
@@ -65,21 +72,21 @@ run("analysis\source_data_analysis.m")
 % Perform Shannon-Fano Source Encoding
 
 shannon_fano_encoded_sequence = shannon_fano_encoding(initial_symbol_sequence);
-show(RESULT, shannon_fano_encoded_sequence);
+show(DEBUG, shannon_fano_encoded_sequence);
 
 
 
 % = = = = = = = = Task 3 = = = = = = = =
 % Analysis of Shannon-Fano Source Encoding
 
-run("analysis\shannon_fano_encoding_analysis.m")
+% run("analysis\shannon_fano_encoding_analysis.m")
 
 
 
 % = = = = = = = = Task 4 = = = = = = = =
 % Analysis of Shannon theorem's condition
 
-run("analysis\shannon_theorem_condition_analysis.m")
+% run("analysis\shannon_theorem_condition_analysis.m")
 
 
 
@@ -92,6 +99,8 @@ run("analysis\shannon_theorem_condition_analysis.m")
 
 
 % = = = = = = = = Task 6 = = = = = = = =
+
+run("analysis\hamming_coding_analysis.m")
 
 
 
@@ -115,14 +124,14 @@ run("analysis\shannon_theorem_condition_analysis.m")
 % = = = = = = = = Task 7 = = = = = = = =
 % Analysis of BPSK spectrum
 
-run("analysis\spectrum_analysis.m")
+% run("analysis\spectrum_analysis.m")
 
 
 
 % = = = = = = = = Task 8 = = = = = = = =
 % Analysis of Probability of > 2 errors occurring
 
-run("analysis\over_two_errors_prob_analysis.m");
+% run("analysis\over_two_errors_prob_analysis.m");
 
 
 
