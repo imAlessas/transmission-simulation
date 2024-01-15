89c756d6cddfd21bfb0becb827f9fe6911d63425
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-01-15 12:34:25 +0200

    finished simulation

--- src/main.m
+++ src/main.m
@@ -12,7 +12,7 @@ import func.*
 % DEBUG: Additional display for variables or comparisons
 % PLOTS: Toggle for generating plots
 RESULT = 1;     % Set to 1 to display result values
-DEBUG = 1;      % Set to 1 to enable additional debugging display
+DEBUG = 0;      % Set to 1 to enable additional debugging display
 PLOTS = 0;      % Set to 1 to generate plots, 0 to disable plots
 SIMULATION = 1; % Set to 1 to compute the simulation
 ANALYSIS = 0;   % Set to 1 to enable the analysis
@@ -42,7 +42,7 @@ U = 1;                              % amplitude BPSK signal [V]
 
 
 % Additional data
-transmitted_symbol_number = randi(100,1);     % number of symbols   
+transmitted_symbol_number = randi(1e5,1);     % number of symbols   
 samples_per_symbol = 500;           % samples per symbol
 r = ceil(log2(codeword_length + 1));% r
 k = codeword_length - r;            % k
@@ -142,7 +142,8 @@ if SIMULATION
     binary_data_matrix = reshape(padded_sequence, k, length(padded_sequence) / k)';
 
     hamming_encoded_matrix = hamming_encoding(binary_data_matrix, codeword_length, k, generation_polynomial);
-    
+    hamming_encoded_matrix = hamming_encoded_matrix';
+
     % unwrap matrix
     hamming_encoded_sequence = hamming_encoded_matrix(:)';
     % show(DEBUG, hamming_encoded_sequence);
@@ -231,7 +232,7 @@ disturbed_signal = BPSK_signal + noise_signal;
 % Perform detection
 
 % Slice recieved signal into segments in each column
-sliced_disturbed_signal = reshape(BPSK_signal, samples_per_symbol, N);
+sliced_disturbed_signal = reshape(disturbed_signal, samples_per_symbol, N);
 
 % Detect the signal with the BPSK threshold
 detected_signal = carrier_signal * sliced_disturbed_signal < 0;
@@ -255,6 +256,9 @@ deinterleaved_sequence = deinterleaving(descrambled_sequence);
 
 show(DEBUG, sum(deinterleaved_sequence ~= hamming_encoded_sequence), "Deinterleaving" )
 
+
+errors_occurred = sum(deinterleaved_sequence ~= hamming_encoded_sequence);
+show(RESULT, errors_occurred, "Errors occurred during the transmission:");
 
 
 
@@ -289,101 +293,13 @@ show(DEBUG, sum(shannon_fano_decoded_sequence ~= initial_symbol_sequence), "Sour
 % = = = = = = = = = = = = = = = = = = = =
 % Correctness checkig
 
+received_symbol_sequence = shannon_fano_decoded_sequence;
+
+show(DEBUG, initial_symbol_sequence);
+show(DEBUG, received_symbol_sequence);
 
 errors_occurred = sum(shannon_fano_decoded_sequence ~= initial_symbol_sequence);
-fprintf("Errors occurred during the transmission:\n %i\n", errors_occurred);
-
-
-
-%%
-
-clc, clearvars, clear, format compact
-
-% Import MATLAB packages
-import communications.*  % used for cyclegen
-
-% Import local packages
-import utils.*
-import func.*
-
-% Display Level Configuration:
-% RESULT: Display values as per guidelines
-% DEBUG: Additional display for variables or comparisons
-% PLOTS: Toggle for generating plots
-RESULT = 1;     % Set to 1 to display result values
-DEBUG = 1;      % Set to 1 to enable additional debugging display
-PLOTS = 0;      % Set to 1 to generate plots, 0 to disable plots
-SIMULATION = 1; % Set to 1 to compute the simulation
-ANALYSIS = 0;   % Set to 1 to enable the analysis
-
-
-
-% = = = = = = = = = = = = = = = = = = = =
-% Initial constant parameters
-
-% source number 7
-alphabet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-probability_vector = [11, 7, 9, 1, 6, 6, 13, 14, 13, 5, 11, 4]/100;
-
-tau = 60e-9;                        % symbol duration time, [s]
-SNR = 8.1;                          % Signal-to-Noise-Ration, [dB]
-                                    % Source Code: Shannon-Fano
-                                    % Error correction code: Cyclic
-generation_polynomial = [...        % z^5 + z^2 + z^0
-    1 0 0 1 0 1]; 
-codeword_length = 31;               % m
-
-f0 = 2.5e+9;                        % carrier frequency [Hz]
-                                    % Modulation: BPSK
-phase_shift = pi;                   % Phase shift [rad]
-U = 1;                              % amplitude BPSK signal [V]
-
-
-
-% Additional data
-transmitted_symbol_number = randi(100,1);     % number of symbols   
-samples_per_symbol = 500;           % samples per symbol
-r = ceil(log2(codeword_length + 1));% r
-k = codeword_length - r;            % k
-
-% Used for the scrambling/descrmabling algorithm
-scrambler_key = randi(2, 1, codeword_length) - 1; 
-
-
-sequence = randi(2,1, 800) - 1
-
-padded_sequence = add_padding_bits(sequence)
-
-padded_matrix = reshape(padded_sequence, k, length(padded_sequence)/k)';
-
-coded_matrix = hamming_encoding(padded_matrix, codeword_length, k, generation_polynomial);
-
-coded_matrix = coded_matrix';
-
-coded_sequence = coded_matrix(:)';
-
-coded_matrix = reshape(coded_sequence, codeword_length, length(coded_sequence) / codeword_length)';
-
-decoded_matrix = hamming_decoding(coded_matrix, codeword_length, k, generation_polynomial);
-
-decoded_matrix = decoded_matrix';
-
-decoded_sequence = decoded_matrix(:)';
-
-
-
-sum(decoded_sequence ~= padded_sequence)
-
-
-
-
-
-
-
-
-
-
-
+show(RESULT, errors_occurred, "Uncorrected errors:");
 
 
 
