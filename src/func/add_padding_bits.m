8918a448344c1111d0852c26b8423d8c25c06b95
imAlessas <alessandrotrigolo@gmail.com>
2024-07-07 16:39:14 +0200

    Update padding_bits function and testing
    The function is generic and can hadle different length of codeword.

--- src/func/add_padding_bits.m
+++ src/func/add_padding_bits.m
@@ -1,45 +1,43 @@
-function padded_sequence = add_padding_bits(compact_sequence)
-% ADD_PADDING_BITS adds padding bits to the end of a compact sequence to meet interleaving requirements.
+function padded_sequence = add_padding_bits(compact_sequence, k, r)
+% ADD_PADDING_BITS Adds padding bits to the end of a compact sequence to meet interleaving requirements.
 %
 % INPUT:
-%   compact_sequence:          A vector representing the compact sequence.
+%   compact_sequence: A vector representing the compact sequence.
+%   k:                An integer representing the divisor used in determining the number of padding bits.
+%   r:                An integer representing the number of bits reserved for storing the padding information.
 %
 % OUTPUT:
-%   padded_sequence:           A vector representing the sequence with added padding bits.
+%   padded_sequence:  A vector representing the sequence with added padding bits.
 
     % Determine the number of bits needed for padding
-    bits_to_pad = count_padding_bits(compact_sequence);
+    bits_to_pad = count_padding_bits(compact_sequence, k, r);
 
     % Convert the number of bits to pad to binary representation
-    binary_padding_value = str2num(dec2bin(bits_to_pad, 5)')';
+    binary_padding_value = str2num(dec2bin(bits_to_pad, r)')';
 
     % Add padding bits to the end of the sequence
-    padded_sequence = [compact_sequence, zeros(1, bits_to_pad - 5), binary_padding_value];
+    padded_sequence = [compact_sequence, zeros(1, bits_to_pad - r), binary_padding_value];
 end
 
 
 
 
-function number_of_padding_bits = count_padding_bits(compact_sequence)
-% COUNT_PADDING_BITS calculates the number of padding bits needed for interleaving.
+function number_of_padding_bits = count_padding_bits(compact_sequence, k, r)
+% COUNT_PADDING_BITS Calculates the number of padding bits needed for interleaving.
 %
 % INPUT:
-%   compact_sequence:          A vector representing the compact sequence.
+%   compact_sequence: A vector representing the compact sequence.
+%   k:                An integer representing the divisor used in determining the number of padding bits.
+%   r:                An integer representing the number of bits reserved for storing the padding information.
 %
 % OUTPUT:
-%   number_of_padding_bits:    The number of padding bits needed.
-
-    % Define the number of bits reserved for storing the padding information
-    storage_bits = 5;
-
-    % Define the divisor used in determining the number of padding bits
-    divisor = 26;
+%   number_of_padding_bits: The number of padding bits needed.
 
     % Calculate the number of padding bits required
-    number_of_padding_bits = divisor - rem(length(compact_sequence), divisor);
+    number_of_padding_bits = k - rem(length(compact_sequence), k);
 
     % Adjust the number of padding bits if it is less than the storage_bits
-    if number_of_padding_bits < storage_bits
-        number_of_padding_bits = number_of_padding_bits + divisor;
+    if number_of_padding_bits < r
+        number_of_padding_bits = number_of_padding_bits + k;
     end
 end
--- src/func/remove_padding_bits.m
+++ src/func/remove_padding_bits.m
@@ -1,31 +1,35 @@
-function compact_sequence = remove_padding_bits(padded_sequence)
-% REMOVE_PADDING_BITS removes padding bits from the end of a padded sequence.
+function compact_sequence = remove_padding_bits(padded_sequence, r)
+% REMOVE_PADDING_BITS Removes padding bits from the end of a padded sequence.
 %
 % INPUT:
-%   padded_sequence:    A vector representing the sequence with padding bits.
+%   padded_sequence: A vector representing the sequence with padding bits.
+%   r:               An integer representing the number of bits reserved for storing the padding information.
 %
 % OUTPUT:
-%   compact_sequence:   A vector representing the sequence with padding bits removed.
+%   compact_sequence: A vector representing the sequence with padding bits removed.
 
     % Call the get_padding_bits function to determine the number of padding bits
-    padding = get_padding_bits(padded_sequence);
+    padding = get_padding_bits(padded_sequence, r);
 
     % Remove the padding bits from the end of the sequence
     compact_sequence = padded_sequence(1 : end - padding);
 end
 
 
-function padding_bits = get_padding_bits(padded_sequence)
-% GET_PADDING_BITS extracts the binary representation of the padding bits from the end of a sequence.
+
+
+function padding_bits = get_padding_bits(padded_sequence, r)
+% GET_PADDING_BITS Extracts the binary representation of the padding bits from the end of a sequence.
 %
 % INPUT:
-%   padded_sequence:    A vector representing the sequence with padding bits.
+%   padded_sequence: A vector representing the sequence with padding bits.
+%   r:               An integer representing the number of bits reserved for storing the padding information.
 %
 % OUTPUT:
-%   padding_bits:       The decimal value representing the extracted padding bits.
+%   padding_bits:    The decimal value representing the extracted padding bits.
 
-    % Extract the last 5 bits from the padded sequence
-    bits = padded_sequence(end-4 : end);
+    % Extract the last r bits from the padded sequence
+    bits = padded_sequence(end - r + 1 : end);
     
     % Convert the binary representation of the bits to a string
     str = '';
